bafb9c7bb0a05245f18e51016674439be9a1d43a
root <root@DESKTOP-0BDT6PR.localdomain>
2021-08-13 21:35:16 -0700

    First commit

---
+++ Source/EscapeBuilding/EscapeBuildingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapeBuildingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEBUILDING_API AEscapeBuildingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
